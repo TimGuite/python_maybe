7dc4f96fa3e07ddc9a16502c83d4ba7c644bbf61
Mapgie Embedded <33968140+Magpie_Embedded@users.noreply.github.com>
2020-01-26 18:46:26 +0000

    Added functionality

--- maybe.py
+++ maybe.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from functools import singledispatch, singledispatchmethod
 
+from typing import List, Callable
+
 
 class Maybe:
     def __eq__(self, x):
@@ -51,16 +53,6 @@ def _(self, other: Nothing) -> bool:
 
 
 @singledispatch
-def from_just(x):
-    raise TypeError("Use this on a Just object")
-
-
-@from_just.register
-def _(x: Just) -> any:
-    return x.value
-
-
-@singledispatch
 def maybe(m, f, default_value):
     raise TypeError("Call with Maybe value")
 
@@ -74,3 +66,104 @@ def _(m: Just, f: callable, default_value: any) -> any:
 def _(m: Nothing, f: callable, default_value: any) -> any:
     return default_value
 
+
+@singledispatch
+def isJust(m):
+    raise TypeError("Call with Maybe value")
+
+
+@isJust.register
+def _(m: Just) -> bool:
+    return True
+
+
+@isJust.register
+def _(m: Nothing) -> bool:
+    return False
+
+
+@singledispatch
+def isNothing(x):
+    raise TypeError("Call with Maybe value")
+
+
+@isNothing.register
+def _(_: Just) -> bool:
+    return False
+
+
+@isNothing.register
+def _(_: Nothing) -> bool:
+    return True
+
+
+@singledispatch
+def from_just(x):
+    raise TypeError("Use this on a Just object")
+
+
+@from_just.register
+def _(x: Just) -> any:
+    return x.value
+
+
+@singledispatch
+def fromMaybe(x, default_value):
+    raise TypeError("Use with a Maybe value")
+
+
+@fromMaybe.register
+def _(m: Just, _) -> any:
+    return from_just(m)
+
+
+@fromMaybe.register
+def _(_: Nothing, default_value: any) -> any:
+    return default_value
+
+
+@singledispatch
+def listToMaybe(_):
+    raise TypeError("Must be called on a list")
+
+
+@listToMaybe.register
+def _(l: list) -> Maybe:
+    if len(l) >= 1:
+        return Just(l[0])
+    else:
+        return Nothing()
+
+
+@singledispatch
+def maybeToList(_):
+    raise TypeError("Use with a Maybe value")
+
+
+@maybeToList.register
+def _(m: Just) -> list:
+    return [from_just(m)]
+
+
+@maybeToList.register
+def _(m: Nothing) -> list:
+    return []
+
+
+@singledispatch
+def catMaybes(_):
+    raise TypeError("Use on a list of Maybe values")
+
+
+@catMaybes.register
+def _(m: list) -> List[any]:
+    """Not ideal but cannot specify a list of Maybes"""
+    try:
+        return [from_just(x) for x in m if isJust(x)]
+    except TypeError:
+        raise TypeError("Please run on a list of Maybes")
+
+
+def mapMaybes(f: Callable[any, maybe], x: List[any]) -> List[any]:
+    output: List[Maybe] = [f(y) for y in x]
+    return catMaybes(output)
--- test_maybe.py
+++ test_maybe.py
@@ -5,12 +5,6 @@ import pytest
 from maybe import *
 
 
-def test_from_just_value_is_value():
-    assert from_just(Just(5)) == 5
-    assert from_just(Just("A string")) == "A string"
-    assert from_just(Just(Nothing())) == Nothing()
-
-
 def test_maybe_applied_to_just_returns_executed_function():
     assert maybe(Just(5), lambda x: x * 2, 1) == 10
     assert maybe(Just([1, 2, 3, 4, 5]), lambda x: x[3:], 0) == [4, 5]
@@ -41,3 +35,69 @@ def test_comparing_non_just_object_to_just_raises_type_error():
         Just(5) == 5
     with pytest.raises(TypeError):
         Just("Some string") == "Not a Just value"
+
+
+def test_is_just_with_just_x_returns_true():
+    assert isJust(Just(5)) == True
+    assert isJust(Just("This is a string")) == True
+    assert isJust(Just(Nothing())) == True
+
+
+def test_is_just_with_nothing_returns_false():
+    assert isJust(Nothing()) == False
+
+
+def test_is_nothing_with_just_x_returns_false():
+    assert isNothing(Just(5)) == False
+    assert isNothing(Just("This is a string")) == False
+    assert isNothing(Just(Nothing())) == False
+
+
+def test_is_just_with_nothing_returns_true():
+    assert isNothing(Nothing()) == True
+
+
+def test_from_just_value_is_value():
+    assert from_just(Just(5)) == 5
+    assert from_just(Just("A string")) == "A string"
+    assert from_just(Just(Nothing())) == Nothing()
+    assert 2 * from_just(Just(10)) == 20
+
+
+def test_from_just_nothing_raises_error():
+    with pytest.raises(TypeError):
+        from_just(Nothing())
+
+
+def test_from_maybe_returns_inner_value_when_just():
+    assert fromMaybe(Just("Hello, World"), "") == "Hello, World"
+    assert fromMaybe(Just(Nothing()), 5) == Nothing()
+
+
+def test_from_maybe_returns_default_value_when_nothing():
+    assert fromMaybe(Nothing(), "nothing") == "nothing"
+
+
+def test_list_to_maybe_with_empty_lists_and_non_empty_lists():
+    assert listToMaybe([9]) == Just(9)
+    assert listToMaybe([]) == Nothing()
+    assert listToMaybe([1, 2, 3]) == Just(1)
+
+
+def test_maybe_to_list_with_nothing_and_just_items():
+    assert maybeToList(Nothing()) == []
+    assert maybeToList(Just(3)) == [3]
+    assert maybeToList(Just([3])) == [[3]]
+
+
+def test_list_to_maybe_and_maybe_to_list_identity_on_singleton_list():
+    assert maybeToList(listToMaybe([5])) == [5]
+    assert maybeToList(listToMaybe([])) == []
+
+
+def test_cat_maybes():
+    assert catMaybes([Just(1), Nothing(), Just(3)]) == [1, 3]
+
+
+def test_map_maybes():
+    assert mapMaybes(lambda x: Just(x), [1, 2, 3]) == [1, 2, 3]
